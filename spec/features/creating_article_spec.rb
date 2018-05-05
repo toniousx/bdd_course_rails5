83a971564444e3a54e843b79e1417e100e12cb08
Tonio Serna <antonelo@gmail.com>
2018-05-05 17:37:54 +0200

    Add articles controller with :new

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -11,6 +11,6 @@ RSpec.describe 'Creating Articles' do
     click_button 'Create Article'
 
     expect(page).to have_content('Article has been created')
-    expect(page.current_path).to eq(articles_path)
+    expect(have_current_path).to eq(articles_path)
   end
 end
