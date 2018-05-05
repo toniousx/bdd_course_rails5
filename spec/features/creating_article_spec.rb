647aa3f47a39a2b569feda5151af46eb434c5c0a
Tonio Serna <antonelo@gmail.com>
2018-05-05 15:05:24 +0200

    add first red/failing tests

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 #:nodoc:
 RSpec.describe 'Creating Articles' do
-  it 'User creates a new article' do
+  scenario 'User creates a new article' do
+    visit '/'
+    click_link "New Article"
+
+    fill_in "Title", with: "Creating a blog"
+    fill_in "Body", with: "Lorem Ipsum"
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
   end
 end
